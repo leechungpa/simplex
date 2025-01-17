6894172e93ea3201691b811902099b4351b257c9
sehee-lim <sehee0706@naver.com>
2025-01-17 00:59:39 +0000

    feat: add name_kwargs

--- solo/args/pretrain.py
+++ solo/args/pretrain.py
@@ -201,4 +201,7 @@ def parse_cfg(cfg: omegaconf.DictConfig):
     if cfg.method == "simclr":
         if omegaconf_select(cfg, "name_kwargs.add_temperature", default=False):
             cfg.name = cfg.name + f"_t{cfg.method_kwargs.temperature}"
+    if cfg.method == "dhel":
+        if omegaconf_select(cfg, "name_kwargs.add_temperature", default=False):
+            cfg.name = cfg.name + f"_t{cfg.method_kwargs.temperature}"
     return cfg
